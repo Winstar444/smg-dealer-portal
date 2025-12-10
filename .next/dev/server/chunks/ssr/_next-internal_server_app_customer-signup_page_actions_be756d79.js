6bc518a5080ed3260215e7532a10c3106b87f22b
ShubhamMehrotra <shubhammehrotra851@gmail.com>
2025-12-10 22:08:40 +0530

    Updated .gitignore

--- .next/dev/server/chunks/ssr/_next-internal_server_app_customer-signup_page_actions_be756d79.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/customer-signup/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_customer-signup_page_actions_be756d79.js.map
--- .next/dev/static/chunks/app_customer-dashboard_page_tsx_6bece898._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_5f7de7cc._.js",
-  "static/chunks/_8bec24a6._.js"
-],
-    source: "dynamic"
-});
